db7e734c40854002ad73d4183898ba84f627fa19
Kanak027 <dagade.kanak@gmail.com>
2025-08-25 15:58:23 +0530

    Signup updated

--- ngo-app/src/pages/SignUp.jsx
+++ ngo-app/src/pages/SignUp.jsx
@@ -18,6 +18,33 @@ const SignUp = () => {
           <label className="block text-gray-600">Password</label>
           <input type="password" className="w-full px-4 py-2 border rounded-lg" />
         </div>
+        <div className="mb-4">
+          <label className="block text-gray-600">College</label>
+          <input type="text" className="w-full px-4 py-2 border rounded-lg" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-600">Phone No</label>
+          <input type="tel" pattern="[0-9]{10}"  className="w-full px-4 py-2 border rounded-lg" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-600">Year of Graduation</label>
+          <input type="number" className="w-full px-4 py-2 border rounded-lg" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-600">Current Year</label>
+          <input type="number" className="w-full px-4 py-2 border rounded-lg" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-600">Field of Study</label>
+          <select className="w-full px-4 py-2 border rounded-lg">
+            <option value="">Select Field</option>
+            <option value="technology">Technology</option>
+            <option value="science">Science</option>
+            <option value="engineering">Engineering</option>
+            <option value="mathematics">Mathematics</option>
+            
+          </select>
+        </div>
         <button type="submit" className="bg-pink-500 text-white px-6 py-3 rounded-full">Sign Up</button>
       </form>
       <p className="text-center mt-4">
